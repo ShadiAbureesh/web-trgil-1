77c69bc2b81360b15f3d44ecbdecdf202d3a9ea7
ShadiAbureesh <Shadiabureesh@gmail.com>
2024-06-09 21:32:03 +0300

    first commit

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/ShadiAbureesh/web-trgil-1.git
+git remote add origin https://github.com/ShadiAbureesh/web-trgil-2.git
 git push -u origin main
